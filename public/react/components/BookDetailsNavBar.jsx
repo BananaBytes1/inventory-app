d54a82ab739c8792b1851bb651dc7f9343515eda
Nicole Rojas <63315208+NPRojas@users.noreply.github.com>
2024-05-25 16:13:09 -0400

    style nav bar and book page

--- public/react/components/BookDetailsNavBar.jsx
+++ public/react/components/BookDetailsNavBar.jsx
@@ -1,12 +1,31 @@
 import { Link } from "react-router-dom";
+import Logo from "../../PageImages/Logo.png"
 
-function BooksDetailsNavBar() {
+function BooksDetailsNavBar({ searchQuery, handleSearchChange }) {
     return (
         <>
-            <Link to="/">Home Page</Link>
-            <Link to="/books">Inventory</Link>
+            <div className="nav-bar">
+                <div className="nav-left">
+                    <img src={Logo} alt= "Logo" className= "logo" id="navBar-logo" />
+                </div>
+
+                <div className="nav-main">
+                <Link to="/" id="nav-a">Home Page</Link>
+                <Link to="/books" id="nav-a">Inventory</Link>
+                </div>
+
+                <div className="nav-right">
+                <input
+                 type="text"
+                 value={searchQuery}
+                 onChange={handleSearchChange}
+                 placeholder="Search"
+                 />
+                </div>
+            </div>
         </>
     )
 }
 
 export default BooksDetailsNavBar
+
